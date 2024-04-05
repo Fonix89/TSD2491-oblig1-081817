fa836a7bd28f73fc21c4a6f73c0481c45b73721f
Fonix89 <157119359+CaptMelis@users.noreply.github.com>
2024-04-05 09:52:22 +0200

    Commit1

---
+++ oblig1-081817/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace oblig1_081817.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
