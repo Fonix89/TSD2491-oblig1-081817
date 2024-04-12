93569c791bfbdea9797ff7830d6335ed5683dcfb
Fonix89 <157119359+CaptMelis@users.noreply.github.com>
2024-04-12 13:01:13 +0200

    Modul/ Commit4
    Lagt inn random generering av emoji-lister

--- oblig1-081817/Models/MatchingGameModel.cs
+++ oblig1-081817/Models/MatchingGameModel.cs
@@ -1,8 +1,17 @@
-﻿namespace oblig1_081817.Models
+﻿using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Timers;
+
+namespace oblig1_081817.Models
 {
     public class MatchingGameModels
     {
-        private readonly List<string> randoms = new List<string>()
+        public int matchesFound = 0;
+
+        static List <string> randomEmoji = new List<string>()
         {
             "🦾", "🦾",
             "😶‍🌫️", "😶‍🌫️",
@@ -14,7 +23,7 @@
             "🍔", "🍔",
         };
 
-        private readonly List<string> animals = new List<string>()
+        static List <string> animalEmoji = new List<string>()
         {
             "🐶", "🐶",
             "🐺", "🐺",
@@ -26,7 +35,7 @@
             "🐭", "🐭",
         };
 
-        private readonly List<string> cars = new List<string>()
+        static List<string> carEmoji = new List<string>()
         {
             "🚗", "🚗",
             "🚓", "🚓",
@@ -37,5 +46,78 @@
             "🚐", "🚐",
             "🚒", "🚒",
         };
+
+        static List<string> weatherEmoji = new List<string>()
+            {
+                "⛅", "⛅",
+                "⛈️", "⛈️",
+                "🌤️", "🌤️",
+                "🌦️", "🌦️",
+                "🌨️", "🌨️",
+                "🌧️", "🌧️",
+                "☁️", "☁️",
+                "🌪️", "🌪️",
+            };
+
+        static Random random = new Random();
+        public List<string> shuffledEmoji = pickRandomEmoji();
+
+        static List<string> pickRandomEmoji()
+        {
+            int randomIndex = random.Next(0, 4);
+
+            switch (randomIndex)
+            {
+                case 0:
+                    return randomEmoji = randomEmoji.OrderBy(items => random.Next()).ToList(); ;
+                case 1:
+                    return animalEmoji = animalEmoji.OrderBy(items => random.Next()).ToList(); ;
+                case 2:
+                    return carEmoji = carEmoji.OrderBy(items => random.Next()).ToList(); ;
+                case 3:
+                    return weatherEmoji = weatherEmoji.OrderBy(items => random.Next()).ToList(); ;
+                default:
+                    throw new Exception("Invalid random index");
+            }
+        }
+        private void SetUpGame()
+        {
+            Random random = new Random();
+            shuffledEmoji = pickRandomEmoji();
+            
+            matchesFound = 0;
+        }
+
+        string lastAnimalFound = string.Empty;
+        string lastDescription = string.Empty;
+
+
+        public void ButtonClick(string animal, string animalDescription)
+        {
+            if (lastAnimalFound == string.Empty)
+            {
+                // First selection of the pari.  Remember it
+                lastAnimalFound = animal;
+                lastDescription = animalDescription;
+            }
+            else if ((lastAnimalFound == animal) && (animalDescription != lastDescription))
+            {
+                // Match found! Reset for the next pair.
+                lastAnimalFound = string.Empty;
+
+                shuffledEmoji = shuffledEmoji
+                    .Select(a => a.Replace(animal, string.Empty))
+                    .ToList();
+                matchesFound++;
+                if (matchesFound == 8)
+                {
+                    SetUpGame();
+                }
+            }
+            else
+            {
+                lastAnimalFound = string.Empty;
+            }
+        }
     }
 }
